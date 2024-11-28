95d852df64b85dc54548a9239e43c4f0ca277661
Gleb Morgachev <gleb@productscience.ai>
2024-11-28 12:48:34 +0800

    Add AST parsing for llama

--- berkeley-function-call-leaderboard/bfcl/model_handler/oss_model/llama.py
+++ berkeley-function-call-leaderboard/bfcl/model_handler/oss_model/llama.py
@@ -19,3 +19,62 @@ class LlamaHandler(OSSHandler):
         formatted_prompt += f"<|start_header_id|>assistant<|end_header_id|>\n\n"
 
         return formatted_prompt
+
+    @overrides
+    def decode_ast(self, result, language="Python"):
+        try:
+            result = result.replace("<|python_tag|>", "")
+            # Llama sometimes separates the function calls with `;` and sometimes with `,`
+            if result.startswith("[") and not result.endswith("]"):
+                result = result + "]"
+            if ";" in result:
+                """
+                "<|python_tag|>{\"name\": \"calc_binomial_probability\", \"parameters\": {\"n\": \"10\", \"k\": \"3\", \"p\": \"0\"}}; {\"name\": \"calc_binomial_probability\", \"parameters\": {\"n\": \"15\", \"k\": \"5\", \"p\": \"0\"}}; {\"name\": \"calc_binomial_probability\", \"parameters\": {\"n\": \"20\", \"k\": \"7\", \"p\": \"0\"}}"
+                """
+                function_calls = result.split(";")
+                function_calls = [json.loads(func_call) for func_call in function_calls]
+            elif "=" in result and "(" in result:
+                res = super().decode_ast(result, language)
+                return res
+            else:
+                """
+                "[\n    {\"name\": \"calculate_permutations\", \"parameters\": {\"n\": \"20\", \"k\": \"5\"}},\n    {\"name\": \"calculate_permutations\", \"parameters\": {\"n\": \"12\", \"k\": \"5\"}},\n    {\"name\": \"calculate_permutations\", \"parameters\": {\"n\": \"10\", \"k\": \"3\"}}\n]"
+                """
+                if result.startswith("[") and not result.endswith("]"):
+                    result = result + "]"
+                function_calls = eval(result)
+                if type(function_calls) == dict:
+                    function_calls = [function_calls]
+
+            decoded_output = []
+            for func_call in function_calls:
+                name = func_call["name"]
+                params = func_call["parameters"]
+                decoded_output.append({name: params})
+
+            return decoded_output
+        except Exception as e:
+            raise e
+
+    @overrides
+    def decode_execute(self, result):
+        print("decode execute")
+        result = result.replace("<|python_tag|>", "")
+        # Llama sometimes separates the function calls with `;` and sometimes with `,`
+        if ";" in result:
+            function_calls = result.split(";")
+            function_calls = [json.loads(func_call) for func_call in function_calls]
+        else:
+            function_calls = eval(result)
+            if type(function_calls) == dict:
+                function_calls = [function_calls]
+
+        execution_list = []
+        for func_call in function_calls:
+            name = func_call["name"]
+            params = func_call["parameters"]
+            execution_list.append(
+                f"{name}({','.join([f'{k}={repr(v)}' for k,v in params.items()])})"
+            )
+
+        return execution_list
